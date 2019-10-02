82d720239821d0010daaf3eb0964403f9ad6700f
kolbecka12 <kolbecka@gmail.com>
2019-10-01 17:20:25 -0700

    second commmit

--- app.js
+++ app.js
@@ -9,17 +9,51 @@ function onReady() {
     let title = NEW_TODO_TEXT.value;
 
     let newLi = document.createElement('li') // create empty <li></li> tag
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
+    newLi.className = "toDoItem";
 
-    let deleteBtn = document.createElement('button');
+    let checkboxLabel = document.createElement('label');
+    checkboxLabel.className = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
+
+    let checkbox = document.createElement('input'); // creates the checkbox
+    checkbox.type = "checkbox";
+    checkbox.className = "mdl-checkbox__input checkbox_styling";
+    checkbox.id = "list-checkbox-1";
+
+
+    // <!-- <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-1">
+    //   <input type="checkbox" id="list-checkbox-1" class="mdl-checkbox__input" checked />
+    // </label> -->
+
+    let deleteBtn = document.createElement('button'); // creates the delete button
     deleteBtn.textContent = "Delete";
+    deleteBtn.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect deleteBtn_styling";
+
+    let primaryContent = document.createElement('span');
+    primaryContent.className = "primary–span";
+
+    let secondaryContent = document.createElement('span');
+    // secondaryContent.className = "mdl-list__item-secondary-action";
+
 
     deleteBtn.addEventListener('click', function(event){
       // this.parentElement represents the button's <li> parent
       TODO_LIST.removeChild(this.parentElement);
 
     })
+
+    // checkboxLabel.appendChild(checkbox);
+    // primaryContent.appendChild(checkboxLabel);
+    // primaryContent.textContent(title);
+    // newLi.appendChild(primaryContent);
+    // newLi.appendChild(secondaryContent);
+
+
+    // thing I tried, but didn't work:
+
+    // newLi.textContent = title;
+    // newLi.appendChild(checkboxLabel);
+    // checkboxLabel.appendChild(checkbox);
+
 
     newLi.textContent = title;
     newLi.appendChild(checkbox);
@@ -32,3 +66,10 @@ function onReady() {
 window.onload = function() {
   onReady();
 };
+
+
+
+
+//keep primary and secondary styling, but try to just get the normal todo function working within the card -- WITHOUT
+//MDL styling. Then add in styling to each element individually. Go back to basics.
+//inspect the checkbox. why is it getting a big margin added around it? solve this first.
